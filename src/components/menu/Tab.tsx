5f85770f4dd400cc5fd164cb08d2a630e7adc637
Dean R <platoisalive@outlook.com>
2022-12-20 07:40:56 +0000

    refactored a little

--- src/components/menu/Tab.tsx
+++ src/components/menu/Tab.tsx
@@ -8,32 +8,48 @@ const Tab: React.FC<{
 	onButtonPopped: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }> = ({ label, onButtonDown, onButtonPopped }) => {
 	const [isDragged, setIsDragged] = useState(false);
+
+	const buttonDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		onButtonDown(e);
+		let drag = false;
+		document.onmousemove = (ee) => {
+			if (
+				!drag &&
+				((ee.clientX - e.clientX) ** 2 + (ee.clientY - e.clientY) ** 2) ** 0.5 >
+					32
+			) {
+				drag = true;
+				setIsDragged(true);
+				onButtonPopped(e);
+			}
+		};
+		document.onmouseup = () => {
+			setIsDragged(false);
+			document.onmousemove = null;
+			document.onmouseup = null;
+		};
+	};
+
 	return (
 		<div
 			className={"bit-button no-pad centered tab".concat(
 				isDragged ? " is-dragged" : ""
 			)}
-			onMouseDown={(e) => {
-				onButtonDown(e);
-				let drag = false;
-				document.onmousemove = (ee) => {
-					if (
-						!drag &&
-						((ee.clientX - e.clientX) ** 2 + (ee.clientY - e.clientY) ** 2) **
-							0.5 >
-							32
-					) {
-						drag = true;
-						setIsDragged(true);
-						onButtonPopped(e);
-					}
-				};
-				document.onmouseup = () => {
-					setIsDragged(false);
-					document.onmousemove = null;
-					document.onmouseup = null;
-				};
-			}}
+			onMouseDown={(e) => buttonDown(e)}
+		>
+			<p className="bit-text">{label}</p>
+		</div>
+	);
+};
+
+export const FloatingTab: React.FC<{
+	label: string;
+	pos: [number, number];
+}> = ({ label, pos }) => {
+	return (
+		<div
+			className="bit-button no-pad centered tab floating"
+			style={{ left: pos[0], top: pos[1] }}
 		>
 			<p className="bit-text">{label}</p>
 		</div>
--- src/components/menu/Tabs.tsx
+++ src/components/menu/Tabs.tsx
@@ -1,13 +1,45 @@
-import React, { useContext } from "react";
-import Tab from "./Tab";
+import React, { useContext, useState } from "react";
+import Tab, { FloatingTab } from "./Tab";
 import "./tabs.css";
 import * as Scenes from "../../game/scenes/GameScene";
 import { GameContext } from "../app/App";
 
 const tabs = () => {
 	const { game } = useContext(GameContext);
+	const [floatingTabText, setFloatingTabText] = useState<string>();
+	const [xy, setXY] = useState<[number, number]>([0, 0]);
+
+	const pop = (
+		e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+		text: string
+	) => {
+		(game?.scene.getScene("GameScene") as Scenes.GameScene)?.playSound("pop");
+		setFloatingTabText(text);
+		document.onmousemove = (ee) => {
+			setXY([
+				ee.clientX - (e.target as HTMLDivElement).clientWidth,
+				ee.clientY - (e.target as HTMLDivElement).clientHeight,
+			]);
+		};
+		document.onmouseup = (ee) => {
+			setXY([0, 0]);
+			setFloatingTabText(undefined);
+		};
+	};
+
+	const boop = (i: number) => {
+		(game?.scene.getScene("GameScene") as Scenes.GameScene)?.playSound("boop", {
+			detune: 0 - i * 100,
+		});
+	};
+
 	return (
 		<div className="tab-container">
+			{floatingTabText ? (
+				<FloatingTab label={floatingTabText} pos={xy} />
+			) : (
+				<></>
+			)}
 			{[
 				"LBL",
 				"SET",
@@ -23,17 +55,8 @@ const tabs = () => {
 				<Tab
 					key={i}
 					label={v}
-					onButtonDown={(e) => {
-						(game?.scene.getScene("GameScene") as Scenes.GameScene)?.playSound(
-							"boop",
-							{ detune: 0 - i * 100 }
-						);
-					}}
-					onButtonPopped={(e) => {
-						(game?.scene.getScene("GameScene") as Scenes.GameScene)?.playSound(
-							"pop"
-						);
-					}}
+					onButtonDown={() => boop(i)}
+					onButtonPopped={(e) => pop(e, v)}
 				/>
 			))}
 		</div>
